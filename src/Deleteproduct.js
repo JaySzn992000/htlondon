0cd88bb4c48854bfb2043520c2e0bd02e0ec74f3
Jay Kumar <jay.k.992000@gmail.com>
2026-03-07 21:02:35 +0530

    updated server.js, collections.js, lotus.js

--- src/Deleteproduct.js
+++ src/Deleteproduct.js
@@ -10,7 +10,7 @@ const handleDelete = async (e) => {
 e.preventDefault();
 
 try {
-const response = await fetch("http://localhost:3001/deletebyname", {
+const response = await fetch("https://omega-zg6z.onrender.com/deletebyname", {
 method: "POST",
 headers: {
 "Content-Type": "application/json",
