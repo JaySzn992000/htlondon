85619dae08c43ea2ba16a44df973e052098a25ba
Jay Kumar <jay.k.992000@gmail.com>
2026-03-07 08:34:09 +0530

    updated server.js, collections.js, lotus.js

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
+// config.js
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: "neondb_owner",
+  host: "ep-twilight-frog-addghbvs-pooler.c-2.us-east-1.aws.neon.tech",
+  database: "neondb",
+  password: "npg_9HdagToMF5Jx",
+  port: 5432,
   ssl: {
-    rejectUnauthorized: false,
-  },
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
-
