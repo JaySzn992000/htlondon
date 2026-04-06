c4b4e10e4cb59025d4a30a86cf6cf8851228c935
Jay Kumar <jay.k.992000@gmail.com>
2026-04-06 07:51:48 +0530

    message

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
-// config.js
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "neondb_owner",
-  host: "ep-twilight-frog-addghbvs-pooler.c-2.us-east-1.aws.neon.tech",
-  database: "neondb",
-  password: "npg_9HdagToMF5Jx",
-  port: 5432,
-  ssl: {
-    rejectUnauthorized: false
-  }
+user: "neondb_owner",
+host: "ep-gentle-moon-adw63t5d-pooler.c-2.us-east-1.aws.neon.tech",
+database: "neondb",
+password: "npg_NU8udI2iTSLt",
+port: 5432,
+ssl: {
+rejectUnauthorized: false
+}
 });
 
 module.exports = pool;
