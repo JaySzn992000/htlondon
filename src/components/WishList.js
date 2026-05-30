cbcd22f85f2d75ddfbaa8fd7660a48212be76110
Jay Kumar <jay.k.992000@gmail.com>
2026-05-30 08:47:34 +0530

    message

--- src/components/WishList.js
+++ src/components/WishList.js
@@ -16,11 +16,24 @@ setWishlist(storedWishlist);
 }, [] );
 
 const handleRemove = (index) => {
+const removedItem = wishlist[index];
+
 const newWishlist = wishlist.filter((_, i) => i !== index);
+
 setWishlist(newWishlist);
 localStorage.setItem("wishlist", JSON.stringify(newWishlist));
 
-window.dispatchEvent(new Event("wishlistUpdated"));
+let storedWishlistStatus =
+JSON.parse(localStorage.getItem("wishlistStatus")) || {};
+
+storedWishlistStatus[removedItem.id] = false;
+
+localStorage.setItem(
+"wishlistStatus",
+JSON.stringify(storedWishlistStatus)
+);
+
+window.dispatchEvent(new Event("storage"));
 };
 
 useEffect(() => {
