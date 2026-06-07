50d086a2e1188ba6457d2f63be7412a524aa3da9
Jay Kumar <jay.k.992000@gmail.com>
2026-06-07 11:42:19 +0530

    message

--- src/headers_footer/header.js
+++ src/headers_footer/header.js
@@ -90,6 +90,10 @@ exclusive offers.</p>
 <h4>Contact Us</h4>
 <li><a>Email: HTLondon@gmail.com</a></li>
 <li><a>Phone: +91 9111574281</a></li>
+
+<li><a>Address : Phoenix Square, Sector 1, <br></br>
+Aimnabad, Bisrakh Jalalpur, Greater Noida, <br></br>
+201318</a></li>
 </ul>
 
 </div>
